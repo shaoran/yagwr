aa63b7c655bf830bef0e4d60c0d18bd4902a89bb
Shaoran <shaoran@sakuranohana.org>
2021-11-17 00:59:25 +0100

    chore: executing the action

--- src/yagwr/webhooks.py
+++ src/yagwr/webhooks.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import re
 
 from http.server import BaseHTTPRequestHandler
 
@@ -97,4 +98,46 @@ async def process_gitlab_request_task(controller):
         request = await queue.get()
         queue.task_done()
 
-        # TODO: process request
+        headers = request.get("headers") or {}
+
+        data_dict = {
+            "path": request.get("path"),
+            "gitlab_token": headers.get("X-Gitlab-Token"),
+            "gitlab_event": headers.get("X-Gitlab-Event"),
+            "gitlab_host": headers.get("Host"),
+        }
+
+        rules = controller.get("rules") or []
+
+        for idx, rule in enumerate(rules):
+            try:
+                match = rule.matches(data_dict)
+            except:
+                log.error("Rule evaluation for rule %d", idx + 1)
+                continue
+
+            if rule.matches(data_dict):
+                try:
+                    await execute_action(request, rule.action, log)
+                except asyncio.CancelledError:
+                    raise
+                except:
+                    log.error(
+                        "Unable to execute action of rule %d", idx + 1, exc_info=True
+                    )
+
+
+async def execute_action(request, action, log):
+    transform_key = lambda key: re.sub(r"[\s-]", "_", key)
+
+    headers = request.get("headers") or {}
+    env = {"YAGWR_" + transform_key(key): str(value) for key, value in headers.items()}
+
+    log.debug("Creating subprocess")
+    proc = await asyncio.create_subprocess_shell(
+        action, stdin=asyncio.subprocess.PIPE, env=env
+    )
+
+    log.debug("Writing stdin with payload")
+    await proc.communicate(input=request.get("body"))
+    log.debug("return code: %s", proc.returncode)
